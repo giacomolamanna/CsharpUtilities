27861697c93786d3f3b2b00a905923b5889bbfef
Giacomo Lamanna <97486319+giacomolamanna@users.noreply.github.com>
2022-01-17 18:08:26 +0100

    Update PropertyGridShowMyProperties.cs

--- PropertyGridShowMyProperties.cs
+++ PropertyGridShowMyProperties.cs
@@ -1,11 +1,26 @@
-public class MyTextBox : TextBox {
+
+//I need to hide some CategoryAttribute in the propertygrid, 
+//and only show some of them. The objects that I have to display in the propertygrid 
+//are objects that I have derived from existing objects, but of these I have to display 
+//only some properties that are in the categories set by me.
+//This is the way!!!
+
+
+//my object
+public class MyTextBox : TextBox 
+{
     public MyProperties MyProperties { get; set; }
-    public MyTextBox() {
+    public MyTextBox() 
+    {
         MyProperties = new MyProperties();
     }
 
 }
-public class MyProperties {
+
+
+//properties of the object
+public class MyProperties 
+{
     [Browsable(true)]
     [ReadOnly(false)]
     [Category("MyCategory")]
@@ -13,5 +28,7 @@ public class MyProperties {
     public double MyProp { get; set; } = 1;
 }
 
+
+//show in propertygrid only my object's properties
 MyTextBox myTextBox = new MyTextBox();
 this.propertyGridControl.SelectedObject = myTextBox.MyProperties;
